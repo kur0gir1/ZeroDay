33341d3098ca046734560b4134877a42ab279d3e
Gilbert Lerion <150149521+kur0gir1@users.noreply.github.com>
2025-05-31 02:25:32 +0800

    updated cursor and added animation

--- zeroday/src/App.jsx
+++ zeroday/src/App.jsx
@@ -3,10 +3,22 @@ import AnimatedRoutes from "./AnimatedRoutes";
 import GlitchOverlay from "./components/glitchOverlay";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./App.css";
+import { useEffect } from "react";
 
 function AppContent() {
   const location = useLocation();
   const isGameRoute = location.pathname === "/game";
+
+  useEffect(() => {
+    if (isGameRoute) {
+      document.body.classList.add("glitch-cursor");
+    } else {
+      document.body.classList.remove("glitch-cursor");
+    }
+    return () => {
+      document.body.classList.remove("glitch-cursor");
+    };
+  }, [isGameRoute]);
 
   return (
     <>
